cc9dc00c663ce56408837a554d36cd82856debec
Sebastian <Sebastian@x230i.fritz.box>
2015-08-03 09:45:15 +0200

    Added Package für die View (GUI)

--- src/client/view/view.java
+++ src/client/view/view.java
@@ -27,17 +27,106 @@ public class view extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        neuBtn = new javax.swing.JButton();
+        globalLbl = new javax.swing.JLabel();
+        delBtn = new javax.swing.JButton();
+        globalChkbox = new javax.swing.JCheckBox();
+        saveBtn = new javax.swing.JButton();
+        loginLbl = new javax.swing.JLabel();
+        tafelLbl = new javax.swing.JLabel();
+        titelLbl = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList();
+        lokaleNachrichtLbl = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        neuBtn.setText("Neu");
+
+        globalLbl.setText("Global");
+
+        delBtn.setText("Löschen");
+
+        saveBtn.setText("Speichern");
+
+        loginLbl.setText("Login");
+
+        tafelLbl.setText("Tafel 1");
+
+        titelLbl.setText("Titel");
+
+        jList1.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane2.setViewportView(jList1);
+
+        lokaleNachrichtLbl.setText("Lokale Nachricht");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(79, 79, 79)
+                .addComponent(neuBtn)
+                .addGap(28, 28, 28)
+                .addComponent(delBtn)
+                .addGap(30, 30, 30)
+                .addComponent(globalLbl)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(globalChkbox)
+                .addGap(18, 18, 18)
+                .addComponent(saveBtn)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lokaleNachrichtLbl))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 81, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(titelLbl)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 405, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(21, 21, 21))
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                            .addComponent(loginLbl)
+                            .addGap(18, 18, 18)
+                            .addComponent(tafelLbl)
+                            .addGap(66, 66, 66)))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(loginLbl)
+                    .addComponent(tafelLbl))
+                .addGap(8, 8, 8)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(titelLbl)
+                    .addComponent(lokaleNachrichtLbl))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1))
+                .addGap(38, 38, 38)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(neuBtn)
+                    .addComponent(delBtn)
+                    .addComponent(globalLbl)
+                    .addComponent(globalChkbox)
+                    .addComponent(saveBtn))
+                .addContainerGap(40, Short.MAX_VALUE))
         );
 
         pack();
@@ -79,5 +168,18 @@ public class view extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton delBtn;
+    private javax.swing.JCheckBox globalChkbox;
+    private javax.swing.JLabel globalLbl;
+    private javax.swing.JList jList1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JLabel loginLbl;
+    private javax.swing.JLabel lokaleNachrichtLbl;
+    private javax.swing.JButton neuBtn;
+    private javax.swing.JButton saveBtn;
+    private javax.swing.JLabel tafelLbl;
+    private javax.swing.JLabel titelLbl;
     // End of variables declaration//GEN-END:variables
 }
